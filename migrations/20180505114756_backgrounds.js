d5a81efeb5fa7581d43bf485f89550e58fdf0d44
Anthony Le Brun <you@example.com>
2018-05-05 11:54:17 +1200

    created database

---
+++ migrations/20180505114756_backgrounds.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('backgrounds', (table) => {
+        table.increments('id').primary()
+        table.string('title')
+        table.string('description')
+        table.string('url')
+        table.string('contributor')
+      })
+    }
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('backgrounds')
+};
