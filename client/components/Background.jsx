adc3e619fd750dcdf66036f67efb481f29f27849
Anthony Le Brun <you@example.com>
2018-06-16 11:15:00 +1200

    have aside in place

--- client/components/Background.jsx
+++ client/components/Background.jsx
@@ -4,17 +4,39 @@ import { fetchBackgrounds } from "../actions";
 import Artwork from "./Artwork";
 import RightColumns from "./RightColumns";
 import LeftColumns from "./LeftColumns";
+import { fetchArtworks } from "../actions/artworks";
 
 class Background extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchBackgrounds());
+    this.props.dispatch(fetchArtworks());
   }
 
   render() {
+    {
+      var oneArtwork = this.props.artworks[this.props.artworkIndex];
+    }
+    {
+      oneArtwork = oneArtwork || "";
+    }
     var divStyle = {
       top: `${this.props.vertical}%`,
       left: `${this.props.left}%`,
       width: "15%"
+    };
+
+    var asideStyle = {
+      position: "absolute",
+      left: "0",
+      top: "0",
+      /* set these so Chrome doesn't return 'auto' from getComputedStyle */
+      width: "200px",
+      height: "200px",
+      background: "transparent",
+      padding: "8px",
+      backgroundImage: `url(${oneArtwork.url})`,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center"
     };
 
     {
@@ -29,10 +51,20 @@ class Background extends React.Component {
           <LeftColumns />
         </div>
         <div className="column is-8" id="maincontent">
-          <div className="backgroundcontainer">
+          <div
+            className="backgroundcontainer"
+            ondragover="drag_over(event)"
+            ondrop="drop(event)"
+          >
             <img src={oneBackground.url} />
             <div style={divStyle} className="artworkcontainer">
-              <Artwork />
+              <aside
+                id="txt2"
+                draggable="true"
+                ondragstart="drag_start(event)"
+                style={asideStyle}
+              />
+              <img src={oneArtwork.url} />
             </div>
           </div>
         </div>
@@ -49,7 +81,9 @@ function mapStateToProps(state) {
     backgrounds: state.backgrounds,
     index: state.index,
     left: state.left,
-    vertical: state.vertical
+    vertical: state.vertical,
+    artworks: state.artworks,
+    artworkIndex: state.artworkIndex
   };
 }
 
